4a1faa0601f676415466e44c59ccb3773a7a332a
Uchi516 <Mateomiyauchi516@gmail.com>
2026-04-08 08:15:44 -0500

    Please provide the code changes or file diffs you would like me to summarize.

--- mainuitemplate.php
+++ mainuitemplate.php
@@ -33,6 +33,13 @@ class mwtheme_default_mainuitemplate extends mwmod_mw_ui2_template_main {
     protected string $custom_css_path = "/res/themes/default/css/custom.css";
 
     /**
+     * Path (relative to public_html) of the theme JS enhancement file.
+     *
+     * @var string
+     */
+    protected string $custom_js_path = "/res/themes/default/js/theme.js";
+
+    /**
      * Load standard UI2 CSS and then append the theme override sheet.
      *
      * @param mwmod_mw_html_manager_css $cssmanager
@@ -43,6 +50,22 @@ class mwtheme_default_mainuitemplate extends mwmod_mw_ui2_template_main {
         $cssmanager->add_item_by_item(
             new mwmod_mw_html_manager_item_css("theme-custom", $this->custom_css_path)
         );
+    }
+
+    /**
+     * Load standard UI2 JS and then append the theme enhancement script.
+     * The script is injected at the bottom of <body> so it runs after the
+     * framework markup is fully parsed.
+     *
+     * @param object $mainUI
+     * @param mwmod_mw_html_manager_js $jsmanager
+     */
+    function add_default_js_scripts_for_main($mainUI, $jsmanager) {
+        parent::add_default_js_scripts_for_main($mainUI, $jsmanager);
+
+        $item = new mwmod_mw_html_manager_item_jsexternal("theme-custom-js", $this->custom_js_path);
+        $item->bottom = true;
+        $jsmanager->add_item_by_item($item);
     }
 }
 ?>
